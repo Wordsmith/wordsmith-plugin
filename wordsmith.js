dd6b95664f48cbbda645fbb051166c420c0e71e0
Rich Hollis <richhollis@gmail.com>
2012-07-27 11:18:53 +0100

    Removed unneccessary settings and improved names of existing settings Fixed issue where clicking outside of selector caused question mark icon to still be shown Improved lookup image sizing by loading lookup image during init

--- wordsmith.js
+++ wordsmith.js
@@ -2,116 +2,156 @@
  * Wordsmith v1.0
  * getwordsmith.co
  *
- * Copyright (c) Rich Hollis, Jess Eddy
- * Available under the BSD and MIT licenses: getwordsmith.co/license/
+ * Copyright (c) Rich Hollis, Jess Eddy 2012
+ * Available under the BSD and MIT licenses: http://getwordsmith.co/license/
  */
-(function ($) {
-    var $question, lastSelection, settings;
-    var host = "http://lookup.getwordsmith.co/";
-    var appName = "wordsmith";
+(function($) {
+     var $question, lastSelection, settings;
+     var appName = "wordsmith";
+     var wordSymbol = "{word}";
+     var lookupImage;
+     var wordsmithSelectorClick = false;
 
-    $.fn[appName] = function (options) {
+     $.fn[appName] = function(options) {
 
-        settings = $.extend({
-            'paddingTop': 6,
-            'maxLength': 18,
-            'width': 600,
-            'height': 400,
-            'lookupMessage': "Lookup Word",
-            'lookupImage': host + "images/Question_Mark.png",
-            'lookupUrl': host + "definition/" + text,
-            'className': "wordsmith"
-        }, options);
+          settings = $.extend({
+               'maxWordLength': 18,
+               'popupWidth': 600,
+               'popupHeight': 400,
+               'lookupImage': "/images/Question_Mark.png",
+               'lookupMessage': "Lookup definition",
+               'lookupUrl': "http://define.getwordsmith.co/" + wordSymbol
+          }, options);
 
-        this.bind("mouseup", mouseup).addClass(settings.className);
-        $(document).bind("mouseup", bodyMouseup);
-    };
+          $(document).on("mouseup", function(event) {
+               mouseUpBody();
+          });
+          this.on("mouseup", function(event) {
+               mouseUp(event)
+          });
+          this.on("mousedown", function(event) {
+               mouseDown(event);
+          });
 
-    function trim(str) {
-        return str.replace(/^\s+|\s+$/g, "");
-    }
+          lookupImage = new Image();
+          lookupImage.src = settings.lookupImage;
+     };
 
-    function getSelected() {
-        var t = '';
-        if (window.getSelection) {
-            t = window.getSelection();
-        } else if (document.getSelection) {
-            t = document.getSelection();
-        } else if (document.selection) {
-            t = document.selection.createRange().text;
-        }
-        return t;
-    }
+     function trim(str) {
+          return str.replace(/^\s+|\s+$/g, "");
+     }
 
-    function bodyMouseup() {
-        if (!$(this).hasClass(settings.className)) {
-            remove();
-        }
-    }
+     function clearSelection() {
+          if (window.getSelection) {
+               if (window.getSelection().empty) { // Chrome
+                    window.getSelection().empty();
+               } else if (window.getSelection().reappNamemoveAllRanges) { // Firefox
+                    window.getSelection().removeAllRanges();
+               }
+          } else if (document.selection) { // IE?
+               document.selection.empty();
+          }
+     }
 
-    function remove() {
-        if ($question) $question.remove();
-        lastSelection = $question = null;
-    }
+     function getSelected() {
+          var t = '';
+          if (window.getSelection) {
+               t = window.getSelection();
+          } else if (document.getSelection) {
+               t = document.getSelection();
+          } else if (document.selection) {
+               t = document.selection.createRange().text;
+          }
+          return t;
+     }
 
-    function mouseup(event) {
-        var sel = getSelected();
-        var text = trim(sel.toString()).toLowerCase();
+     function mouseUpBody() {
+          if (!wordsmithSelectorClick) {
+               remove();
+          }
+          wordsmithSelectorClick = false;
+     }
 
-        if (text.length == 0 || text.length >= settings.maxLength) {
-            remove();
-            event.preventDefault();
-            return false;
-        }
+     function remove() {
+          if ($question) $question.remove();
+          lastSelection = $question = null;
+     }
 
-        if (text.length > 0) {
+     function mouseDown(event) {
+          clearSelection();
+          remove();
+          wordsmithSelectorClick = true; // mouse event originated inside a wordsmith selector
+     }
 
-            var range = sel.getRangeAt(0);
-            var $span = $('<span/>');
+     function mouseUp(event) {
+          var sel = getSelected();
+          var text = trim(sel.toString()).toLowerCase();
 
-            var newRange = document.createRange();
-            newRange.setStart(sel.focusNode, range.endOffset);
-            newRange.insertNode($span[0]); // using 'range' here instead of newRange unselects or causes flicker on chrome/webkit
+          if (text.length == 0 || text.length >= settings.maxWordLength) {
+               remove();
+               event.preventDefault();
+               return false;
+          }
 
-            var x = $span.offset().left;
-            var y = $span.offset().top;
+          if (text.length > 0) {
 
-            if (lastSelection) {
-                if (lastSelection.x == x && lastSelection.y == y) {
-                    $span.remove();
+               var range = sel.getRangeAt(0);
+               var $span = $('<span/>');
+
+               var newRange = document.createRange();
+               newRange.setStart(sel.focusNode, range.endOffset);
+               newRange.insertNode($span[0]); // using 'range' here instead of newRange unselects or causes flicker on chrome/webkit
+
+               var x = $span.offset().left;
+               var y = $span.offset().top;
+
+               if (lastSelection) {
+                    if (lastSelection.x == x && lastSelection.y == y) {
+                         $span.remove();
+                         event.preventDefault();
+                         return false;
+                    }
+               }
+
+               remove();
+
+               var style = "margin: 0; padding: 0; position: absolute" + "; width: " + lookupImage.width + "; height: " + lookupImage.height;
+               $question = $('<div id="' + appName + '-question" style="' + style + '"><a href="#" id="' + appName + '-lookup" title="' + settings.lookupMessage + '"><img src="' + settings.lookupImage + '"/></a></div>');
+
+               lastSelection = {
+                    sel: sel,
+                    x: x,
+                    y: y
+               };
+
+               $span.parent().append($question[0]);
+
+               // prevent us from being removed by body mouseupatt
+               $('#' + appName + '-lookup').on("mousedown", function(event) {
+                    wordsmithSelectorClick = true;
                     event.preventDefault();
                     return false;
-                }
-            }
+               });
 
-            remove();
-            $question = $('<div id="' + appName + '-question"><a href="#" id="' + appName + '-lookup" title="' + settings.lookupMessage + '"><img src="' + settings.lookupImage + '"/></a></div>');
+               $question.css({
+                    left: x - (lookupImage.width / 1.5),
+                    top: y - (lookupImage.height / 1.5)
+               });
+               $span.remove();
 
-            lastSelection = {
-                sel: sel,
-                x: x,
-                y: y
-            };
+               var $lookup = $('#' + appName + '-lookup');
+               $lookup.click(function(e) {
+                    var lookupUrl = settings.lookupUrl.replace(wordSymbol, text);
+                    var left = (screen.width - settings.popupWidth) / 2;
+                    var top = (screen.height - settings.popupHeight) / 2;
+                    window.open(lookupUrl, appName, "width=" + settings.popupWidth + ",height=" + settings.popupHeight + ",left=" + left + ",top=" + top + ",location=1" + ",scrollbars=1");
+                    e.preventDefault();
+                    return false;
+               });
 
-            $span.parent().append($question[0]);
-
-            $question.css({
-                left: x - $question.width(),
-                top: y - ($question.height() - settings.paddingTop)
-            });
-            $span.remove();
-
-            var $lookup = $('#' + appName + '-lookup');
-            $lookup.click(function (e) {
-                var left = (screen.width - settings.width) / 2;
-                var top = (screen.height - settings.height) / 2;
-                window.open(settings.lookupUrl, appName, "width=" + settings.width + ",height=" + settings.height + ",left=" + left + ",top=" + top + ",location=1" + ",scrollbars=1");
-                e.preventDefault();
-                return false;
-            });
-
-            event.preventDefault();
-            return false;
-        }
-    }
+               event.preventDefault();
+               return false;
+          }
+     }
 })(jQuery);
+
